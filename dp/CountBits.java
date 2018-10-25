67561781df7d301eff399aa8e08123e8499702b0
Debdeep Basu <debdeepbasucal@hotmail.com>
2018-10-24 21:30:56 -0400

    Add CountBits

---
+++ dp/CountBits.java
@@ -0,0 +1,9 @@
+class CountBits {
+    public int[] countBits(int num) {
+        int[] result = new int[num+1];
+        for (int i = 0; i <= num; i++) {
+            result[i] = result[i>>1] + i%2;
+        }
+        return result;
+    }
+}
